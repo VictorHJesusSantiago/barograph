5db28e15aab076dfdef44c0bef05008af2361274
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-09 19:04:02 -0300

    Add configuration for raster processing and notifications, and Sphinx documentation setup

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = "Barograph"
+copyright = "2026, Barograph Contributors"
+author = "Barograph Contributors"
+release = "0.1.0"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "xarray": ("https://docs.xarray.dev/en/stable", None),
+}
+
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
